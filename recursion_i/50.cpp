8105715e24755207c02aeafa380390e7eb76dd96
satouyuuko <subeibei92@gmail.com>
2021-08-30 00:32:24 +0800

    LeetCode 50

---
+++ recursion_i/50.cpp
@@ -0,0 +1,17 @@
+// Pow(x, n)
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ public:
+  double myPow(double x, int n) {
+    return n >= 0 ? myPowImp(x, n) : 1 / myPowImp(x, n);
+  }
+
+ private:
+  double myPowImp(double x, int n) {
+    if (n == 0) return 1;
+    return myPowImp(x * x, n / 2) * (n % 2 ? x : 1);
+  }
+};
